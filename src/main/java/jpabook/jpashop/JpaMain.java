e6d834ab4a61ddb361ae730571778422c2819911
Khalin94 <mpzx1102@gmail.com>
2022-03-24 02:01:42 +0900

    단방향 설계 - 설계 시 최대한 단방향으로 설계되도록 하는 것이 좋다. 양방향 매핑은 어플리케이션 개발 시 필요할 때 만든다. n:1 에서 n 부분에 @ManyToOne, @JoinColumn 어노테이션을 붙혀준다. 1 부분에는 따로 하지 않아도 된다.

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Member;
+import com.sun.org.apache.xpath.internal.operations.Or;
+import jpabook.jpashop.domain.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,8 +20,38 @@ public class JpaMain {
             member.setName("hello");
             member.setCity("LA");
             member.setStreet("45Ave");
-
             em.persist(member);
+
+            Order order = new Order();
+            order.setOrderDate(LocalDateTime.now());
+            order.setStatus(OrderStatus.ORDER);
+            order.setMember(member);
+            em.persist(order);
+
+            Item item = new Item();
+            item.setName("item1");
+            item.setPrice(1000);
+            item.setStockQuantity(5);
+            em.persist(item);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            orderItem.setItem(item);
+            orderItem.setOrderPrice(2000);
+            orderItem.setCount(2);
+            em.persist(orderItem);
+
+            OrderItem findOrderItem = em.find(OrderItem.class, orderItem.getId());
+            Item item1 = findOrderItem.getItem();
+            Order order1 = findOrderItem.getOrder();
+            Member member1 = order1.getMember();
+
+            System.out.println("=====================");
+            System.out.println(findOrderItem.getItem().getPrice() + " : " + findOrderItem.getOrderPrice());
+            System.out.println(item1.getStockQuantity());
+            System.out.println(order1.getMember().getStreet());
+            System.out.println(member1.getZipcode());
+            System.out.println("=====================");
 
             tx.commit();
         }catch (TransactionRequiredException e){
